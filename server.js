cd0ca6efda5965d501d93355aeb11606383f977e
Luiza0607 <110295222+Luiza0607@users.noreply.github.com>
2023-03-01 19:43:25 +0100

    add server.js

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const cors = require('cors');
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
+
+app.listen(8000, () => {
+	console.log('Server is running on port: 8000');
+});
